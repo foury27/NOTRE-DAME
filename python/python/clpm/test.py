7c14905fcc8f409a9eac73e22601c4e0db19dc9b
foury27 <palamy27@gmail.com>
2015-06-25 09:55:17 -0400

    this is my recover

--- python/python/clpm/test.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'foury'
-import numpy as np
-import cv2
-a=cv2.imread("")
-down=cv2.pyrDown(a)
-down=cv2.pyrDown(down)
-down2=cv2.pyrDown(down)
-cv2.imwrite("/Users/foury/Documents/github/amygit/FR/FR_SR/FR_SR/testimg2/high/high_downsampled/high/20.jpg",down)
-cv2.imwrite("/Users/foury/Documents/github/amygit/FR/FR_SR/FR_SR/testimg2/high/low_downsampled/low/20.jpg",down2)
--- python/python/deep learning/test.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'foury'
-class amy(object):
-    def __init__(self,hobby,men):
-        self.hobby=hobby
-        self.men=men
-    def prt(self):
-        print self.hobby,self.men
-
-
